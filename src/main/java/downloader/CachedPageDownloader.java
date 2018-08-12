ad6f3b9c00e4d55dd264f93af82886466aa27edf
Karol Nowak <poczta@karolnowak.net>
2018-08-12 20:34:02 +0200

    Add cached UrlFinder
    Which doesn't seem to help

--- src/main/java/downloader/CachedPageDownloader.java
+++ src/main/java/downloader/CachedPageDownloader.java
@@ -5,18 +5,18 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class CachedPageDownloader implements PageDownloader {
     private final PageDownloader pageDownloader;
-    private final ConcurrentHashMap<URL, String> pages = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<URL, String> cache = new ConcurrentHashMap<>();
 
     public CachedPageDownloader(PageDownloader pageDownloader) {
         this.pageDownloader = pageDownloader;
     }
     @Override
     public String downloadContent(URL url) {
-        if (pages.containsKey(url)) {
-            return pages.get(url);
+        if (cache.containsKey(url)) {
+            return cache.get(url);
         }
         String content = pageDownloader.downloadContent(url);
-        pages.put(url, content);
+        cache.put(url, content);
         return content;
     }
 }
